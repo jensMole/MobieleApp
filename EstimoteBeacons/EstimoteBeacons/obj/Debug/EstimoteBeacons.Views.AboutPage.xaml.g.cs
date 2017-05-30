7132d21780c789f8b74ef96262af29766545fc56
jensMole <jensmolenaers@hotmail.com>
2017-05-30 12:09:54 +0200

    Kleine aanpassing about page

--- EstimoteBeacons/EstimoteBeacons/obj/Debug/EstimoteBeacons.Views.AboutPage.xaml.g.cs
+++ EstimoteBeacons/EstimoteBeacons/obj/Debug/EstimoteBeacons.Views.AboutPage.xaml.g.cs
@@ -11,8 +11,8 @@
 namespace EstimoteBeacons.Views {
     
     
-    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("C:\\Users\\Sven\\Desktop\\MobieleApp\\EstimoteBeacons\\EstimoteBeacons\\Views\\AboutPage." +
-        "xaml")]
+    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("C:\\Users\\jensie\\Desktop\\MobieleApp\\EstimoteBeacons\\EstimoteBeacons\\Views\\AboutPag" +
+        "e.xaml")]
     public partial class AboutPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
